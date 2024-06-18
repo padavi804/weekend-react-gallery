371e48b2dd607c0c2cf2639c8b0cbb9ce89de607
Patrick Davison <davisonpatrickj@gmail.com>
2024-06-17 20:10:34 -0500

    added list css and import

--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -1,9 +1,9 @@
 import GalleryItem from '../GalleryItem/GalleryItem';
-
+import './GalleryList.css'
 
 export default function GalleryList  ({id, pic}) {
     return (
-        <div data-testid="galleryList">
+        <div data-testid="galleryList" className='galleryList'>
             {pic.map (pic => (
             <GalleryItem key={id} pic={pic}/>
         ))}
