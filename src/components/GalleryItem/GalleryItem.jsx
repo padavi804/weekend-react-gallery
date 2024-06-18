33b68d15a091b436aeba279255511db67951811c
Patrick Davison <davisonpatrickj@gmail.com>
2024-06-17 21:54:22 -0500

    photos and description toggling is working

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
-import {useState, useEffect} from 'react';
-
+import {useState} from 'react';
+import axios from 'axios';
 
 
 export default function GalleryItem ({pic}){
     
 const [photoDescription, setDescription] = useState(false);
+const [likeCount, setLikeCount] = useState(0);
     // useState variable for toggle
 const toggleDesc = () => {
     setDescription(!photoDescription);
@@ -14,10 +15,23 @@ const toggleDesc = () => {
 
 
 // like button axios.put
-
+const likePhoto = (id) => {
+    axios.put(`/api/gallery/likes/${id}`)
+    .then(response =>{
+        setLikeCount( + 1);
+    })
+    .catch(error => {
+        console.log('cannot like photo error', error);
+    });
+};
+   
   return (
-    <div data-testid="galleryItem">           
-        <img src={pic.url} alt="Gallery Image" />    
+    <div data-testid="galleryItem"> 
+        <div onClick={() => toggleDesc(pic.id)}>
+            {photoDescription ? pic.description  : <img src={pic.url} alt={pic.title} /> }         
+        </div> 
+        <button onClick={() => likePhoto(pic.id)}>Like</button><p>Likes: {likeCount}</p>
+
     </div>
   );
 };
